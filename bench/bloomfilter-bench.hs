4d7db26e827905695540a2e3214cd8a737c0535d
Duncan Coutts <duncan@well-typed.com>
2025-09-11 11:55:39 +0100

    Extend benchmark program to compare the original bloomfilter package too
    We want to quantify how much better it is.

--- bench/bloomfilter-bench.hs
+++ bench/bloomfilter-bench.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP            #-}
+{-# LANGUAGE PackageImports #-}
 module Main (main) where
 
 import           Control.DeepSeq (NFData)
@@ -10,9 +12,17 @@ import           GHC.Conc (pseq)
 import           System.Random
 import           System.Random.Stateful
 
-import qualified Data.BloomFilter.Blocked as B.Blocked
-import qualified Data.BloomFilter.Classic as B.Classic
-import qualified Data.BloomFilter.Hash as B
+-- From this package.
+import qualified "bloomfilter-blocked" Data.BloomFilter.Blocked as B.Blocked
+import qualified "bloomfilter-blocked" Data.BloomFilter.Classic as B.Classic
+import qualified "bloomfilter-blocked" Data.BloomFilter.Hash as B
+
+#ifdef ORIGINAL_BLOOMFILTER
+-- From the original bloomfilter package.
+import qualified "bloomfilter" Data.BloomFilter as B.Original
+import qualified "bloomfilter" Data.BloomFilter.Easy as B.Original
+import qualified "bloomfilter" Data.BloomFilter.Hash as B.Original
+#endif
 
 main :: IO ()
 main =
@@ -68,10 +78,17 @@ data BenchBloomImpl b k =
 
 benchBloomImpls :: [SomeBenchBloomImpl]
 benchBloomImpls =
-  [ SomeBenchBloomImpl (benchBloomImpl_classic (Proxy :: Proxy Word64))
-  , SomeBenchBloomImpl (benchBloomImpl_blocked (Proxy :: Proxy Word64))
-  , SomeBenchBloomImpl (benchBloomImpl_classic (Proxy :: Proxy Bytes100))
-  , SomeBenchBloomImpl (benchBloomImpl_blocked (Proxy :: Proxy Bytes100))
+  [
+#ifdef ORIGINAL_BLOOMFILTER
+    SomeBenchBloomImpl (benchBloomImpl_original (Proxy :: Proxy Word64)),
+#endif
+    SomeBenchBloomImpl (benchBloomImpl_classic (Proxy :: Proxy Word64)),
+    SomeBenchBloomImpl (benchBloomImpl_blocked (Proxy :: Proxy Word64)),
+#ifdef ORIGINAL_BLOOMFILTER
+    SomeBenchBloomImpl (benchBloomImpl_original (Proxy :: Proxy Bytes100)),
+#endif
+    SomeBenchBloomImpl (benchBloomImpl_classic (Proxy :: Proxy Bytes100)),
+    SomeBenchBloomImpl (benchBloomImpl_blocked (Proxy :: Proxy Bytes100))
   ]
 
 benchBloomImpl_classic :: (B.Hashable a, Uniform a, Typeable a)
@@ -94,10 +111,26 @@ benchBloomImpl_blocked proxy =
       lookupBloom    = lookupBloom_blocked
     }
 
+#ifdef ORIGINAL_BLOOMFILTER
+benchBloomImpl_original :: (B.Original.Hashable a, Uniform a, Typeable a)
+                        => Proxy a -> BenchBloomImpl B.Original.Bloom a
+benchBloomImpl_original proxy =
+    BenchBloomImpl {
+      nameBloom      = "bloomfilter:Data.BloomFilter@"
+                    ++ show (typeRep proxy),
+      constructBloom = constructBloom_original,
+      lookupBloom    = lookupBloom_original
+    }
+#endif
+
 {-# SPECIALISE benchBloomImpl_classic  :: Proxy Word64   -> BenchBloomImpl B.Classic.Bloom Word64 #-}
 {-# SPECIALISE benchBloomImpl_classic  :: Proxy Bytes100 -> BenchBloomImpl B.Classic.Bloom Bytes100 #-}
 {-# SPECIALISE benchBloomImpl_blocked  :: Proxy Word64   -> BenchBloomImpl B.Blocked.Bloom Word64 #-}
 {-# SPECIALISE benchBloomImpl_blocked  :: Proxy Bytes100 -> BenchBloomImpl B.Blocked.Bloom Bytes100 #-}
+#ifdef ORIGINAL_BLOOMFILTER
+{-# SPECIALISE benchBloomImpl_original :: Proxy Word64   -> BenchBloomImpl B.Original.Bloom Word64 #-}
+{-# SPECIALISE benchBloomImpl_original :: Proxy Bytes100 -> BenchBloomImpl B.Original.Bloom Bytes100 #-}
+#endif
 
 --
 -- Construction
@@ -115,10 +148,24 @@ constructBloom_blocked !n !fpr !g0 =
   let (!salt, !g1) = uniform g0 in
     B.Blocked.unfold (B.Blocked.sizeForFPR fpr n) salt (nextElement n) (g1, 0)
 
+#ifdef ORIGINAL_BLOOMFILTER
+constructBloom_original :: (B.Original.Hashable a, Uniform a)
+                        => Int -> Double -> StdGen -> B.Original.Bloom a
+constructBloom_original !n !fpr !g0 =
+    B.Original.unfold hash nbits (nextElement n) (g0, 0)
+  where
+    hash = B.Original.cheapHashes nhashes
+    (nbits, nhashes) = B.Original.suggestSizing n fpr
+#endif
+
 {-# SPECIALISE constructBloom_classic  :: Int -> Double -> StdGen -> B.Classic.Bloom Word64 #-}
 {-# SPECIALISE constructBloom_classic  :: Int -> Double -> StdGen -> B.Classic.Bloom Bytes100 #-}
 {-# SPECIALISE constructBloom_blocked  :: Int -> Double -> StdGen -> B.Blocked.Bloom Word64 #-}
 {-# SPECIALISE constructBloom_blocked  :: Int -> Double -> StdGen -> B.Blocked.Bloom Bytes100 #-}
+#ifdef ORIGINAL_BLOOMFILTER
+{-# SPECIALISE constructBloom_original :: Int -> Double -> StdGen -> B.Original.Bloom Word64 #-}
+{-# SPECIALISE constructBloom_original :: Int -> Double -> StdGen -> B.Original.Bloom Bytes100 #-}
+#endif
 
 --
 -- Lookup
@@ -142,11 +189,25 @@ lookupBloom_blocked !b !n !g0 = go g0 0
         Nothing            -> ()
         Just (x, (g', i')) -> B.Blocked.elem x b `pseq` go g' i'
 
+#ifdef ORIGINAL_BLOOMFILTER
+lookupBloom_original :: Uniform a => B.Original.Bloom a -> Int -> StdGen -> ()
+lookupBloom_original !b !n !g0 = go g0 0
+  where
+    go !g !i =
+      case nextElement n (g, i) of
+        Nothing            -> ()
+        Just (x, (g', i')) -> B.Original.elem x b `pseq` go g' i'
+#endif
+
 
 {-# SPECIALISE lookupBloom_classic  :: B.Classic.Bloom Word64   -> Int -> StdGen -> () #-}
 {-# SPECIALISE lookupBloom_classic  :: B.Classic.Bloom Bytes100 -> Int -> StdGen -> () #-}
 {-# SPECIALISE lookupBloom_blocked  :: B.Blocked.Bloom Word64   -> Int -> StdGen -> () #-}
 {-# SPECIALISE lookupBloom_blocked  :: B.Blocked.Bloom Bytes100 -> Int -> StdGen -> () #-}
+#ifdef ORIGINAL_BLOOMFILTER
+{-# SPECIALISE lookupBloom_original :: B.Original.Bloom Word64   -> Int -> StdGen -> () #-}
+{-# SPECIALISE lookupBloom_original :: B.Original.Bloom Bytes100 -> Int -> StdGen -> () #-}
+#endif
 
 --
 -- Utils
@@ -163,6 +224,9 @@ nextElement !n (!g, !i)
 -- | 100 long strings of bytes, used as Bloom filter keys
 newtype Bytes100 = Bytes100 ByteString
   deriving newtype B.Hashable
+#ifdef ORIGINAL_BLOOMFILTER
+  deriving newtype B.Original.Hashable
+#endif
 
 instance Uniform Bytes100 where
   uniformM g = Bytes100 <$> uniformByteStringM 100 g
